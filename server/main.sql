c2c2ec59fb1bd4863c55acc5ecb0ea996b3a6a2c
fariq <tonykriss2323@gmail.com>
2025-11-21 22:51:59 +0700

    first

---
+++ server/main.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE tag (
+    tag_id SERIAL PRIMARY KEY,
+    tag_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE post (
+    post_id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    media_type VARCHAR(20) CHECK (media_type IN ('video', 'photo', 'none')),
+    description TEXT
+);
+
+CREATE TABLE post_tag (
+    post_id INTEGER NOT NULL REFERENCES post(post_id) ON DELETE CASCADE,
+    tag_id INTEGER NOT NULL REFERENCES tag(tag_id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, tag_id)
+);
+
+CREATE TABLE time (
+    time_id SERIAL PRIMARY KEY,
+    post_id INTEGER NOT NULL REFERENCES post(post_id) ON DELETE CASCADE,
+    year INTEGER CHECK (year >= 0)
+);
+
+CREATE TABLE post_like (
+    user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    post_id INTEGER NOT NULL REFERENCES post(post_id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, post_id) 
+);
+
